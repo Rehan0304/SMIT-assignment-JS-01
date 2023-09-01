9c0af30e53ddc759c8c21d4f6a62273f1371b36d
Muhammad rehan <109660304+Rehan0304@users.noreply.github.com>
2023-09-01 13:55:07 +0500

    assignment

---
+++ index.js
@@ -0,0 +1,68 @@
+//! chapter 01
+
+//? Q1
+
+// alert("assalam o alaikum!");
+
+//? Q2
+
+// alert("Error! pleae enter avalid passoword");
+
+//? Q3
+
+// alert("Welcome to JS land... \n Happy coding!" );
+
+//? Q4
+
+// alert("Welcome to JS land");
+// alert("Happy coding!");
+
+//! chapter 02
+
+//? Q 01
+
+// var userName;
+
+//? Q 02
+
+// var myName="Muhammad rehan";
+
+//? Q 03
+
+// var message;
+// message="Hello world";
+// alert(message);
+
+//? Q 04
+
+// var myName="Rehan";
+// var myAge="I am 20 years old";
+// var enrolledCourse="Certified Mobile application development";
+// alert(myName );
+// alert(myAge);
+// alert(enrolledCourse);
+
+//? Q 05
+
+// var food="PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(food);
+
+//? Q 06
+
+// var email="example@examle.com"
+// alert("My email address is " + email); 
+
+//? Q 07
+
+// var book=" A smarter way to learn JavaScript " ;
+// alert("I am trying to learn from a book " + book) 
+
+//? Q 08
+
+// document.write("Yeh! i can write HTML content through javascript")
+
+//? Q 09
+
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
